5c2eb52b1b44bd24f98fc3ca11ad42debb92edd7
kmindev <kankgm0324@gmail.com>
2023-05-07 14:44:21 +0900

    Comment: "필요없는 import 제거"

--- backend/src/main/java/com/backend/order/OrderRepository.java
+++ backend/src/main/java/com/backend/order/OrderRepository.java
@@ -2,9 +2,6 @@ package com.backend.order;
 
 import com.backend.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
     Order findFirstByMemberOrderByOrderIdDesc(Member member);

9d33ea504a780278433ad901f3303a2e4e79fd9b
mrcsbin <mrcsbin@naver.com>
2023-05-12 15:34:56 +0900

    feat: 로그인상태를 관리하는 store 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./modules/cart";
+import loginReducer from "./modules/login";
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
+    login: loginReducer,
   },
 });
 

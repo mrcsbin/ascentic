c14f1ffc52fe7d75afdf2220be1bbca32f345746
kmindev <kangkm0324@gmailcom>
2023-05-16 13:18:20 +0900

    Refactor: "주문페이지 refactor"
    1. redux toolgit 라이브러리를 이용하여 order 페이지의 state를 관리하도록 변경
    2. 컴포넌트 재사용을 위한 styled component
    3. api재사용성을 위한 api를 분리 (export)

--- frontend/src/components/order/FinalPayment.js
+++ frontend/src/components/order/FinalPayment.js
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState } from "react";
+import { useSelector } from "react-redux";
+import { requestOrder } from "../../api/OrderApi";
+import { getCookie } from "../../utils/Cookies";
+import { styled } from "styled-components";
 
 // 최종 결제금액
-function FinalPayment(props) {
+const FinalPayment = (props) => {
   // 동의 관련 state
   const [checkValues, setCheckValues] = useState({
     check1: false,
@@ -28,42 +32,88 @@ function FinalPayment(props) {
     });
   };
 
+  // 전역 state
+  const orderInformation = useSelector((state) => state.order.orderInformation);
+  const shipInfo = useSelector((state) => state.order.shipInfo);
+  const paymentMethod = useSelector((state) => state.order.paymentMethod);
+
+  // 상품정보
+  const prods = props.prods;
+  const products = props.products;
+  const accessToken = getCookie("accessToken");
+
+  // 상품들의 총금액 구하기
+  const prodSumPrice = prods.reduce((acc, cur) => {
+    return acc + cur.prodPrice;
+  }, 0);
+
+  // order 요청 데이터
+  const requestData = {
+    // memberId: token, // 주문자 id
+    orderEmail: orderInformation.email + "@" + orderInformation.domain, // 주문자 이메일
+    orderName: orderInformation.name, // 주문자 이름
+    orderTel: orderInformation.tel, // 주문자 연락처
+    shipName: shipInfo.shipName, // 수령인
+    shipTel: shipInfo.shipTel, // 연락처
+    shipMainAddress: shipInfo.mainAddress, // 메인주소
+    shipSubAddress: shipInfo.subAddress, // 상세주소
+    shipMessage: shipInfo.shipMessage, // 배송메시지
+    orderPayment: paymentMethod, // 결제 수단
+    orderPaymentInfo: "결제정보입니다!", // 카드번호, 결제관련 정보
+    orderPaymentState: false, // true: 결제완료, flase: 결제실패
+    orderState: "결제완료", // 결제완료, 배송준비중, 배송중, 배송완료
+    orderPriceSum: prodSumPrice, // 상품 총 금액
+    shipCharge: 2500, // 배송비
+    discount: 0, //할인 금액
+  };
+
   // 구매하기 버튼 클릭 이벤트
-  const buySubmit = (e) => {
-    e.preventDefault();
+  const buySubmit = async () => {
     const { check1, check2, check3, check4 } = checkValues;
 
     if (check1 && check2 && check3 && check4) {
-      props.buySubmit(); // 결제 진행
       alert("결제를 진행하겠습니다.");
+      try {
+        const orderNum = await requestOrder(accessToken, requestData, products);
+        console.log("주문 성공");
+      } catch (e) {
+        console.error(e);
+      }
     } else {
       alert("모두 동의가 필요합니다.");
     }
   };
 
   return (
-    <div>
-      <div className="final_sub_title">최종 결제금액</div>
+    <FinalPay>
+      <FinalSubTitle>최종 결제금액</FinalSubTitle>
       {/* 결제정보 */}
-      <div className="final_content">
+      <FinalContent>
         <div>
           <div>주문금액</div>
-          <div>95,000원</div>
+          <div>{requestData.orderPriceSum.toLocaleString()}원</div>
         </div>
         <div>
           <div>배송비</div>
-          <div>+2,500원</div>
+          <div>+{requestData.shipCharge.toLocaleString()}원</div>
         </div>
         <div>
           <div>할인 금액</div>
-          <div>0원</div>
+          <div>{requestData.discount.toLocaleString()}원</div>
         </div>
         <div>
           <div>총 금액</div>
-          <div>97,500원</div>
+          <div>
+            {(
+              requestData.orderPriceSum +
+              requestData.shipCharge -
+              requestData.discount
+            ).toLocaleString()}
+            원
+          </div>
         </div>
-      </div>
-      <div className="final_agree">
+      </FinalContent>
+      <FinalAgree>
         <div>
           <input
             type="checkbox"
@@ -109,12 +159,79 @@ function FinalPayment(props) {
           ></input>{" "}
           (필수)위 주문의 상품, 가격, 할인, 배송 정보에 동의합니다.
         </div>
-      </div>
-      <button id="buy_btn" onClick={buySubmit}>
-        구매하기
-      </button>
-    </div>
+      </FinalAgree>
+      <button onClick={() => buySubmit()}>구매하기</button>
+    </FinalPay>
   );
-}
+};
 
 export default FinalPayment;
+
+const FinalSubTitle = styled.div`
+  margin-left: 10px;
+  border: none;
+  width: 562px;
+  height: 27px;
+  font-weight: 700;
+  font-size: 24px;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 28px;
+
+  border-top: 1px solid #d9d9d9;
+  transform: matrix(1, 0, -0.01, 1, 0, 0);
+`;
+
+const FinalContent = styled.div`
+  > div {
+    border-bottom: 1px solid #d9d9d9;
+    transform: matrix(1, 0, -0.01, 1, 0, 0);
+    /* background-color: aqua; */
+    width: 562px;
+    height: 53px;
+    margin-left: 10px;
+    margin-bottom: 15px;
+    font-size: 20px;
+  }
+
+  > div > div:nth-child(1) {
+    margin-top: 15px;
+    float: left;
+  }
+
+  > div > div:nth-child(2) {
+    margin-top: 15px;
+    float: right;
+  }
+
+  > div:nth-child(4) {
+    font-weight: 700;
+    font-size: 28px;
+  }
+`;
+
+const FinalAgree = styled.div`
+  width: 562px;
+  height: 185px;
+  margin-left: 20px;
+  margin-top: 26px;
+
+  > div:nth-child(1) {
+    margin-bottom: 30px;
+  }
+
+  > div {
+    margin-bottom: 5px;
+  }
+`;
+
+const FinalPay = styled.div`
+  > button {
+    margin-left: 20px;
+    background-color: black;
+    width: 529px;
+    height: 80px;
+    font-size: 25px;
+    color: white;
+  }
+`;

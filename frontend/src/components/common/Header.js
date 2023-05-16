29604bb767a6afce74ad772aee9144c2a8de578d
minnonanan <user@Minbook-pro.local>
2023-05-16 09:40:53 +0900

    design: 헤더 소메뉴, 검색창

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -4,13 +4,27 @@ import { Link } from "react-router-dom";
 import iconUser from "../../assets/iconUser.svg";
 import iconBag from "../../assets/iconBag.svg";
 import iconSearch from "../../assets/iconSearch.svg";
+import arrow from "../../assets/menu_arrow.svg";
 import { getCookie, setCookie, removeCookie } from "../../utils/Cookies";
 
 //HSM
 //RouteTest.js 에 임시로 연결
+//submenu Link "/"으로 한 것 변경 필요
 
 const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(!!getCookie("accessToken"));
+  // 검색창
+  const [showSearch, setShowSearch] = useState(false);
+  // 소메뉴 펼치기
+  const [showMenu1, setShowMenu1] = useState(false);
+  // 검색창 켜기
+  const handleshowSearch = () => {
+    setShowSearch(true);
+  };
+  // 검색창 끄기
+  const handlehideSearch = () => {
+    setShowSearch(false);
+  };
 
   function handleLogout() {
     removeCookie("accessToken");
@@ -32,41 +46,107 @@ const Header = () => {
         </div>
         {/* flexbox 자식 컨테이너 #2 */}
         <ul className="navbarMenu">
-          <li>
-            <Link
-              to="/storemain"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              체험
-            </Link>
+          <li className="menu2-1">
+            <div className="menu2-1-box">
+              <Link
+                to="/storemain"
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                }}
+              >
+                체험
+              </Link>
+            </div>
+            {/* <img src={arrow} alt="arrow"></img> */}
+            <div className="submenu sub2-1">
+              <div className="sub-content">
+                <ul>
+                  <li>
+                    <Link to="/">구독 소개</Link>
+                  </li>
+                  <li>
+                    <Link to="/">취향 테스트</Link>
+                  </li>
+                  <li>
+                    <Link to="/">구독 신청</Link>
+                  </li>
+                  <li>
+                    <Link to="/">구독 관리</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </li>
-          <li>
-            <Link
-              to="/storemain"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              스토어
-            </Link>
+          <li className="menu2-2">
+            <div className="menu2-2-box">
+              <Link
+                to="/storemain"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                스토어
+              </Link>
+            </div>
+            <div className="submenu sub2-2">
+              <div className="sub-content">
+                <ul>
+                  <li>
+                    <Link to="/">필터</Link>
+                  </li>
+                  <li>
+                    <Link to="/">검색 결과</Link>
+                  </li>
+                  <li>
+                    <Link to="/">제품별 카테고리</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </li>
-          <li>
-            <Link
-              to="/proddetail"
-              style={{ textDecoration: "none", color: "black" }}
-            >
-              커뮤니티
-            </Link>
+          <li className="menu2-3">
+            <div className="menu2-3-box">
+              <Link
+                to="/proddetail"
+                style={{ textDecoration: "none", color: "black" }}
+              >
+                커뮤니티
+              </Link>
+            </div>
+            <div className="submenu sub2-3">
+              <div className="sub-content">
+                <ul>
+                  <li>
+                    <Link to="/">뉴스</Link>
+                  </li>
+                  <li>
+                    <Link to="/">공지사항</Link>
+                  </li>
+                  <li>
+                    <Link to="/">이벤트</Link>
+                  </li>
+                  <li>
+                    <Link to="/">커뮤니티 글</Link>
+                  </li>
+                  <li>
+                    <Link to="/">리뷰</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </li>
         </ul>
         {/* flexbox 자식 컨테이너 #3 */}
         <ul className="navbarIcon">
           <li>
-            <img src={iconSearch} alt="iconSearch"></img>
+            <img
+              src={iconSearch}
+              alt="iconSearch"
+              onClick={handleshowSearch}
+              style={{ cursor: "pointer" }}
+            ></img>
           </li>
           <li>
-            <Link
-              to="/login"
-              style={{ textDecoration: "none" }}
-            >
+            <Link to="/login" style={{ textDecoration: "none" }}>
               <img src={iconUser} alt="iconMyPage"></img>
             </Link>
           </li>
@@ -84,8 +164,36 @@ const Header = () => {
           ) : null}
         </ul>
       </nav>
+      {/* 검색창 */}
+      {showSearch && (
+        <div className="search">
+          <div className="search-wrap">
+            {/* 0512 form 삭제하기 -> css 재작업 */}
+            <form>
+              <input
+                type="text"
+                name="search"
+                placeholder="원하는 향을 검색하세요."
+                minLength="1"
+                maxLength="10"
+              ></input>
+              <button type="submit" class="search-img">
+                <img
+                  src={iconSearch}
+                  alt="iconSearch"
+                  style={{ cursor: "pointer" }}
+                ></img>
+              </button>
+              <button className="close-btn" onClick={handlehideSearch}>
+                닫기
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
 
 export default Header;
+// 구현 후 display none hover시 살아남 , z인덱스 x인덱스 이용해서 기본값을 보이는거에서 -> hover시 나오게 ,시간차 설정

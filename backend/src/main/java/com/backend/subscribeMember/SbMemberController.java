ffac8dceaf817dad3bf35ad67afa71f1a6310c39
hansic7 <hansic0124@gmail.com>
2023-05-16 11:11:53 +0900

    feat : 구독관리 기능 추가

--- backend/src/main/java/com/backend/subscribeMember/SbMemberController.java
+++ backend/src/main/java/com/backend/subscribeMember/SbMemberController.java
@@ -1,10 +1,8 @@
 package com.backend.subscribeMember;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping
 @RestController
@@ -13,9 +11,19 @@ public class SbMemberController {
 
     private final SbMemberServiceImpl sbMemberService;
 
-
-    @PostMapping("startSubscribe")
+    @PostMapping("/startSubscribe")
     public void startSubscribe(@RequestBody SubscribeMemberDto subscribeMemberDto){
         sbMemberService.sbMemberAdd(subscribeMemberDto);
     }
+
+    @GetMapping("/lastSbMember")
+    public LastSbMemberDTO lastSubscribeMember(){
+        return sbMemberService.getLastSbMemberByMemberId();
+    }
+
+    @GetMapping("/endSubscribe")
+    public void endSubscribe() {
+        sbMemberService.endSubscription();
+    }
+
 }
--- backend/src/main/java/com/backend/subscribeMember/SbMemberService.java
+++ backend/src/main/java/com/backend/subscribeMember/SbMemberService.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface SbMemberService {
     public void sbMemberAdd(SubscribeMemberDto subscribeMemberDto);
+
+    public LastSbMemberDTO getLastSbMemberByMemberId();
+
 }

c87e420a20a124426e9a4aa656d54fc261fc9ed5
hansic7 <hansic0124@gmail.com>
2023-05-22 13:29:36 +0900

    feat :  실행시 기본 insert 문 (sbMember, sbProd, wish)

---
+++ backend/src/main/resources/subscribeMember.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_subscribe_member (sb_member_num, member_id, sb_start_date, sb_end_date, sb_member_name, sb_member_tel, sb_main_addr, sb_sub_addr, sb_ship_message, sb_pay, sb_payment_day, sb_price, taste_result) VALUES
+(1, 'member1', '2023-05-01', '2023-05-31', 'Subscriber 1', '1234567890', '123 Main St', 'Apt 4B', 'Leave at the door', 'Paid', 1, 10000, 'Sweet'),
+(2, 'member2', '2023-05-02', '2023-06-01', 'Subscriber 2', '0987654321', '456 Secondary St', 'Suite 2A', 'Ring the bell', 'Unpaid', 2, 15000, 'Sour'),
+(3, 'member3', '2023-05-03', '2023-06-02', 'Subscriber 3', '1122334455', '789 Tertiary St', 'Unit 3C', 'Knock three times', 'Paid', 3, 20000, 'Bitter'),
+(4, 'member4', '2023-05-04', '2023-06-03', 'Subscriber 4', '9988776655', '321 Quaternary St', 'Bldg 4D', 'Sign for delivery', 'Unpaid', 4, 25000, 'Salty'),
+(5, 'member5', '2023-05-05', '2023-06-04', 'Subscriber 5', '8877665544', '654 Quinary St', 'Dept 5E', 'Leave in mailroom', 'Paid', 5, 30000, 'Umami');
---
+++ backend/src/main/resources/wish.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_wish (wish_num, member_id, prod_num) VALUES
+(1, 'member1', 1),
+(2, 'member2', 2),
+(3, 'member3', 3),
+(4, 'member4', 4),
+(5, 'member5', 5);

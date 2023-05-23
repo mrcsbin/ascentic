88d50962b85b7377b80e0755b9c7ee5ea6931950
kmindev <kangkm0324@gmailcom>
2023-05-23 15:13:32 +0900

    Fix: "구독상품 리뷰, 평점 등록 오류 해결"

--- backend/src/main/java/com/backend/subscribesend/repository/SubscribeSendRepository.java
+++ backend/src/main/java/com/backend/subscribesend/repository/SubscribeSendRepository.java
@@ -10,12 +10,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface SubscribeSendRepository extends JpaRepository<SubscribeSend, Integer> {
-
-    @Transactional
-    @Query(value="update tb_subscribe_send SET sb_send_score = :sb_send_score, " +
-            "sb_send_review = :sb_send_review where sb_send_num = :sb_send_num",
-            nativeQuery=true)
-    void updateReview(@Param("sb_send_num") Integer sb_send_num, @Param("sb_send_score") Integer sb_send_score, @Param("sb_send_review") String sb_send_review);
+//    @Transactional
+//    @Query(value="update tb_subscribe_send SET sb_send_score = :sb_send_score, " +
+//            "sb_send_review = :sb_send_review where sb_send_num = :sb_send_num",
+//            nativeQuery=true)
+//    void updateReview(@Param("sb_send_num") Integer sb_send_num, @Param("sb_send_score") Integer sb_send_score, @Param("sb_send_review") String sb_send_review);
 
     List<SubscribeSend> findAllBySubscribeMember(SubscribeMember subscribeMember);
 }

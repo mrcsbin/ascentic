c7617721a000b7d380442ac1b687fea90f5fbae6
Oing52 <shjo4921@gmail.com>
2023-05-26 15:23:04 +0900

    Fix: 구독 상품 더미데이터 sql문 수정

--- backend/src/main/resources/subscribeProd.sql
+++ backend/src/main/resources/subscribeProd.sql
@@ -1,20 +1,70 @@
-INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro)
-VALUES ('Lavender', '19.99', 'Experience the soothing aroma of lavender');
--- 2번 더미 데이터
-INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro)
-VALUES ('Sandalwood', '24.99', 'Indulge in the warm and woody scent of sandalwood');
--- 3번 더미 데이터
-INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro)
-VALUES ('Rose', '16.99', 'Discover the romantic fragrance of rose petals');
+-- animal
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Ambergris', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 앰버그리스향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Castoreum', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 캐스토리움향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Musk', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 머스크향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
 
--- 4번 더미 데이터
-INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro)
-VALUES ('Vanilla', '21.99', 'Enjoy the sweet and comforting scent of vanilla');
+-- Watery&Powdery
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Aldehydal', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 알데히드향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Vanilla', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 바닐라향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Benzoin', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 벤조인향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
 
--- 5번 더미 데이터
-INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro)
-VALUES ('Lime', '18.99', 'Awaken your senses with the refreshing citrus aroma');
+-- Woody
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Sandalwood', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 샌달우드향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Bark', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 바크향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Cedarwook', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 시더우드향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
 
--- 6번 더미 데이터
-INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro)
-VALUES ('Aldehydal', '23.99', 'Immerse yourself in the enchanting fragrance of jasmine');
+-- Mossy
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Oak moss', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 오크모스향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Tree moss', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 트리모스향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+
+-- Herbal&Green
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Lavender', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 라벤더향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Rosemary', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 로즈마리향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Tea tree', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 티트리향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+
+-- Floral
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Rose', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 로즈향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Orange blossom', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 오렌지블러섬향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Geranium', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 제라니움향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+
+-- Citrus
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Bergamot', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 베르가못향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Lemon', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 레몬향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Lime', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 라임향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+
+-- Fruity
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Apple', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 애플향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Peach', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 피치향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Strawberry', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 스트로베리향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+
+-- Special
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Clove', '22900', '향수, 핸드크림, 바디워시 세가지 상품으로 구성된 클로브향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Cinnamon', '22900', '향초, 섬유향수, 샴푸 세가지 상품으로 구성된 시나몬향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+INSERT INTO tb_subscribe_prod (sp_scent, sp_price, sp_intro, sp_image)
+VALUES ('Tabacco', '22900', '디퓨저, 핸드크림, 섬유향수 세가지 상품으로 구성된 타바코향의 패키지입니다. 에이센틱과 함께 일상을 향기로 채워보세요.', 'exp_package_Ambergris.webp');
+

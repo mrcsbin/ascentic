0d3bb52970c3dfa3f59b8c74c5faae139e3233c0
Oing52 <shjo4921@gmail.com>
2023-05-29 16:18:03 +0900

    Feat: 관리자페이지 구독상품관리 조회, 수정, 삭제, 추가 기능 구현, 정렬 및 페이지네이션 완료 Design: 관리자페이지 구독상품관리 스타일 작업

--- frontend/src/admin/pages/AdminSubscribeManagement.js
+++ frontend/src/admin/pages/AdminSubscribeManagement.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import SbProductManagement from "../components/SubscribeManagement/AdminSbProductManagement";
+import styled from "styled-components";
 
 function AdminSubscribeManagement() {
+  const [spActionMode, setSpActionMode] = useState(1);
   return (
-    <div>
-      <h2>구독 관리 페이지</h2>
-      <p>구독이 좋다~</p>
-    </div>
+    <Wrapper>
+      <ActionBtnbox>
+        <button
+          className={spActionMode === 1 ? "activeBtn" : "Btn"}
+          onClick={() => setSpActionMode(1)}
+        >
+          구독 주문 관리
+        </button>
+        <button
+          className={spActionMode === 2 ? "activeBtn" : "Btn"}
+          onClick={() => setSpActionMode(2)}
+        >
+          구독 상품 관리
+        </button>
+      </ActionBtnbox>
+      <div>
+        {spActionMode === 1 && <div />}
+        {spActionMode === 2 && <SbProductManagement />}
+      </div>
+    </Wrapper>
   );
 }
+const Wrapper = styled.div`
+  width: 85%;
+  float: right;
+`;
+const ActionBtnbox = styled.div`
+  display: flex;
+  width: 100%;
+  height: fit-content;
+  justify-content: flex-start;
+  align-items: center;
+  border-bottom: 1px solid black;
+
+  button {
+    width: 20%;
+    height: 50px;
+    text-align: center;
+    margin: 0;
+    font-size: 1.2rem;
+    font-weight: 500;
+    color: black;
+    background-color: white;
+    border: 0;
+    cursor: pointer;
+  }
+  .activeBtn,
+  button:hover {
+    color: white;
+    background-color: black;
+  }
+`;
 
 export default AdminSubscribeManagement;

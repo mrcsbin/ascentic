6494c67ef8672dfdecced0e35cd0063d2cd274be
Oing52 <shjo4921@gmail.com>
2023-06-03 01:53:07 +0900

    Feat: 관리자페이지 구독주문관리 주문이력에 따른 보기옵션 기능 추가 구현 및 주문이력 조회 구현

--- frontend/src/admin/components/SubscribeManagement/SbSendProductSelect.js
+++ frontend/src/admin/components/SubscribeManagement/SbSendProductSelect.js
@@ -10,9 +10,11 @@ const SbSendProductSelect = ({
 }) => {
   const [nowSelect, setNowSelect] = useState(sbProduct);
   const [sbProdList, setSbProdList] = useState([]);
+  const [sbProdview, setSbProdview] = useState([]);
   const [prodloading, setProdloading] = useState(false);
   const [category, setCategory] = useState(sbMember.tasteResult);
   const [tasteRes, setTasteRes] = useState("");
+  const [radioOption, setRadioOption] = useState("주문이력없음");
   const Categories = [
     {
       name: "전체보기",
@@ -58,7 +60,24 @@ const SbSendProductSelect = ({
   const handleCategory = (e) => {
     setCategory(e.target.value);
   };
+  const handleRadio = (e) => {
+    setRadioOption(e.target.value);
+  };
+  const filterRadioOption = (sbProdList) => {
+    if (radioOption === "주문이력없음") {
+      setSbProdview(
+        sbProdList.filter((p) => p.sbProdMemberReviewDto.length === 0)
+      );
+    } else if (radioOption === "전체") {
+      setSbProdview(sbProdList);
+    } else if (radioOption === "주문이력있음") {
+      setSbProdview(
+        sbProdList.filter((p) => p.sbProdMemberReviewDto.length !== 0)
+      );
+    }
+  };
 
+  const star = [1, 2, 3, 4, 5];
   function addComma(num) {
     var regexp = /\B(?=(\d{3})+(?!\d))/g;
     return num.toString().replace(regexp, ",");
@@ -68,22 +87,24 @@ const SbSendProductSelect = ({
       setProdloading(true);
       try {
         const res = await axios.get(
-          `http://localhost:8080/adminSbMemberRecord?memberId=${sbMember.memberId}&scentNoteName=${category}`
+          `http://localhost:8080/subsProduct/adminSbMemberRecord?memberId=${
+            sbMember.memberId
+          }&scentNoteName=${escape(category)}`
         );
         const result = await axios.get(
           `http://localhost:8080/adminMemberTestResult?memberId=${sbMember.memberId}`
         );
         setSbProdList(res.data);
         setTasteRes(result.data);
-        console.log(res.data);
-        console.log(result.data);
+        filterRadioOption(res.data);
       } catch (e) {
         console.log(e);
       }
       setProdloading(false);
     };
     fetchProducts();
-  }, [category]);
+    console.log(sbProdview);
+  }, [category, radioOption]);
 
   // 대기 중일 때
   if (prodloading) {
@@ -122,39 +143,105 @@ const SbSendProductSelect = ({
               <option value={c.text}>{c.name}</option>
             ))}
           </select>
+          <div className="radiobox">
+            <label>
+              <input
+                type="radio"
+                name="recordoption"
+                value="주문이력없음"
+                onChange={handleRadio}
+                checked={radioOption === "주문이력없음"}
+              />
+              주문이력없음
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="recordoption"
+                value="전체"
+                onChange={handleRadio}
+                checked={radioOption === "전체"}
+              />
+              전체
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="recordoption"
+                value="주문이력있음"
+                onChange={handleRadio}
+                checked={radioOption === "주문이력있음"}
+              />
+              주문이력있음
+            </label>
+          </div>
         </OptionBox>
+        {}
         <ProductContainer>
           <tr className="productTitle">
             <th>구독상품선택</th>
-            <th>총 {sbProdList.length}건</th>
+            <th>총 {sbProdview.length}건</th>
           </tr>
           <ProductItemList>
-            {sbProdList.length === 0 ? (
+            {sbProdview.length === 0 ? (
               <div className="noResult">결과가 없습니다.</div>
             ) : (
-              sbProdList.map((sbproduct) => (
+              sbProdview.map((sbprod) => (
                 <SbProdBox
-                  className={nowSelect === sbproduct ? "active" : ""}
-                  onClick={() => setNowSelect(sbproduct)}
+                  className={nowSelect === sbprod ? "active" : ""}
+                  onClick={() => {
+                    nowSelect === sbprod
+                      ? setNowSelect("")
+                      : setNowSelect(sbprod);
+                  }}
                 >
                   <InlineContent>
-                    <div>ID: {sbproduct.sbProdNum}</div>
-                    <div>{sbproduct.scentNoteName} Note</div>
+                    <div>ID: {sbprod.sbProdNum}</div>
+                    <div>{sbprod.scentNoteName} Note</div>
                   </InlineContent>
                   <ImgBox>
                     <img
-                      src={`/images/${sbproduct.sbProdImage}`}
+                      src={`http://localhost:8080/images/${sbprod.sbProdImage}`}
                       alt="sbProdImage"
                     />
                   </ImgBox>
-                  <BlockContent>{sbproduct.scentName} 향</BlockContent>
-                  <BlockContentS>{sbproduct.sbProdIntro}</BlockContentS>
+                  <BlockContent>{sbprod.scentName} 향</BlockContent>
+                  <BlockContentS>{sbprod.sbProdIntro}</BlockContentS>
                   <InlineContent>
-                    <div>{addComma(sbproduct.sbProdPrice)}원</div>
-                    <div>재고: {sbproduct.sbProdStock}개</div>
+                    <div>{addComma(sbprod.sbProdPrice)}원</div>
+                    <div>재고: {sbprod.sbProdStock}개</div>
                   </InlineContent>
+                  {sbprod.sbProdMemberReviewDto.length !== 0 && (
+                    <>
+                      <RiviewBox>
+                        <div className="reviewdate">
+                          이전주문결제일:{" "}
+                          {sbprod.sbProdMemberReviewDto[0].sbSendPayDate}
+                        </div>
+                      </RiviewBox>
+                      {sbprod.sbProdMemberReviewDto[0].score ? (
+                        <RiviewBox>
+                          <div className="reviewscore">
+                            {star.map((s) =>
+                              s <= sbprod.sbProdMemberReviewDto[0].score ? (
+                                <>✭</>
+                              ) : (
+                                <>☆</>
+                              )
+                            )}
+                            &nbsp;&nbsp;
+                          </div>
+                          <div className="review">
+                            {sbprod.sbProdMemberReviewDto[0].review}
+                          </div>
+                        </RiviewBox>
+                      ) : (
+                        <RiviewBox>리뷰 없음</RiviewBox>
+                      )}
+                    </>
+                  )}
                   <Buttonbox>
-                    {nowSelect === sbproduct ? (
+                    {nowSelect === sbprod ? (
                       <button
                         className="cancelbtn"
                         onClick={() => setNowSelect("")}
@@ -164,7 +251,7 @@ const SbSendProductSelect = ({
                     ) : (
                       <button
                         className="btn"
-                        onClick={() => setNowSelect(sbproduct)}
+                        onClick={() => setNowSelect(sbprod)}
                       >
                         선택
                       </button>
@@ -185,11 +272,11 @@ const SbSendProductSelect = ({
 };
 
 const ModalBackground = styled.div`
-  position: fixed;
-  top: -2%;
-  left: -44%;
-  width: 100vw;
-  height: 100vh;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.4);
   z-index: 1100;
 `;
@@ -222,13 +309,16 @@ const ModalContainer = styled.div`
 const OptionBox = styled.div`
   width: 100%;
   text-align: center;
-  margin-top: 20px;
+  margin-top: 10px;
   select {
     width: 40%;
     height: 40px;
     margin: 0 auto;
     font-size: 1.1rem;
     font-weight: 500;
+  }
+  .radiobox {
+    margin: 10px auto;
   }
 `;
 const TasteResBox = styled.div`
@@ -242,7 +332,7 @@ const TasteResBox = styled.div`
   > div:nth-child(2) {
     font-size: 1.2rem;
     line-height: 2;
-    font-weight: 500;
+    font-weight: 600;
   }
   > div {
     font-size: 1rem;
@@ -311,6 +401,7 @@ const SbProdBox = styled.div`
   padding: 10px;
   width: 500px;
   border-bottom: 1px solid black;
+  cursor: pointer;
 `;
 const InlineContent = styled.div`
   div {
@@ -341,6 +432,31 @@ const BlockContentS = styled.div`
   font-size: 0.9rem;
   line-height: 1.2;
   word-break: keep-all;
+`;
+const RiviewBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  .reviewdate {
+    line-height: 1.5;
+    font-size: 0.9rem;
+    font-weight: 500;
+    color: red;
+  }
+  .reviewscore {
+    margin-left: 10px;
+    line-height: 1.5;
+    font-size: 1rem;
+    font-weight: 600;
+  }
+  .review {
+    margin-left: 10px;
+    line-height: 1.5;
+    font-size: 1rem;
+    font-weight: 500;
+  }
 `;
 const Buttonbox = styled.div`
   display: flex;

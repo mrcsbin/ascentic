baf7814dd224bf27544f3ec3307c0e2de4b9836b
mrcsbin <mrcsbin@naver.com>
2023-06-02 13:02:44 +0900

    "fix: 카카오 로그인 API 수정"

--- frontend/src/components/login/KakaoLogin.js
+++ frontend/src/components/login/KakaoLogin.js
@@ -1,48 +1,31 @@
 import React from "react";
 import { useEffect } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { getMemberInfo } from "../../api/MemberApi";
+import { getCookie, setCookie } from "../../utils/Cookies";
 
-// const kakaoLogin = (code) => {
-//   return function (dispatch, getState, { history }) {
-//     axios({
-//       method: "GET",
-//       url: `http://3.35.208.142/oauth/callback/kakao?code=${code}`,
-//     })
-//       .then((res) => {
-//         console.log(res); // 토큰이 넘어올 것임
-
-//         const ACCESS_TOKEN = res.data.accessToken;
-
-//         localStorage.setItem("token", ACCESS_TOKEN);    //예시로 로컬에 저장함
-
-//         history.replace("/main") // 토큰 받았았고 로그인됐으니 화면 전환시켜줌(메인으로)
-
-//         }.catch((err) => {
-//         console.log("소셜로그인 에러", err);
-//         window.alert("로그인에 실패하였습니다.");
-//         history.replace("/login"); // 로그인 실패하면 로그인화면으로 돌려보냄
-//         }
-//     }
-// };
-
-const KakaoLogin = (props) => {
+const KakaoLogin = () => {
+  const navigate = useNavigate();
   const code = new URL(window.location.href).searchParams.get("code");
 
   useEffect(() => {
     const login = async () => {
-      await axios
-        .get(`http://localhost:8080/oauth/kakao?code=${code}`)
-        .then((res) => {
-          console.log(res);
-          localStorage.setItem("token", res.headers.authorization);
-          //   window.location.href = "/";
-        })
-        .catch((e) => {
-          console.log("안됨");
+      const response = await axios.get(
+        `http://localhost:8080/oauth/kakao?code=${code}`
+      );
+      if (response.data) {
+        const { accessToken, refreshToken } = response.data;
+        const expires = new Date(Date.now() + 30 * 60 * 1000);
+        setCookie("accessToken", accessToken, { expires });
+        setCookie("refreshToken", refreshToken, { expires });
+        await getMemberInfo(getCookie("accessToken")).then(() => {
+          window.location.replace("/");
         });
+      }
     };
     login();
-  }, [props.history]);
+  }, []);
 };
 
 export default KakaoLogin;

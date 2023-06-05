c20b00fe109b8146e62dc3dbcb47ace23c4adfa0
NaSung <2201skgotjd@gmail.com>
2023-06-05 10:38:30 +0900

    fix : Admin 이벤트 및 뉴스 수정하기 고장난거 고침

--- frontend/src/admin/pages/AdminEventNews.js
+++ frontend/src/admin/pages/AdminEventNews.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/AdminEventNews.css";
 import EventList from "../components/EventNews/EventList";
 import NewsList from "../components/EventNews/NewsList";
@@ -8,12 +8,22 @@ import styled from "styled-components";
 
 function AdminEventNews() {
   const params = useParams();
+  const [postEdit, setpostEdit] = useState(null);
+
+  const handleEdit = (postEdit) => {
+    params.category = "post"; // Update params.category to "post"
+    setpostEdit(postEdit);
+  };
+
+  useEffect(() => {
+    console.log("postEdit:", postEdit);
+  }, [postEdit]);
 
   return (
     <Wrap>
-      {params.category === "event" && <EventList />}
-      {params.category === "news" && <NewsList />}
-      {params.category === "post" && <WritePost />}
+      {params.category === "event" && <EventList handleEdit={handleEdit} />}
+      {params.category === "news" && <NewsList handleEdit={handleEdit} />}
+      {params.category === "post" && <WritePost postEdit={postEdit} />}
     </Wrap>
   );
 }

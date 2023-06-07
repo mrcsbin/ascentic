5b76ef23e41ba6f0830a6c6f189dc43041714919
mrcsbin <mrcsbin@naver.com>
2023-06-07 23:13:20 +0900

    "style: 아이디 찾기 ui 변경"

--- frontend/src/pages/FindId.js
+++ frontend/src/pages/FindId.js
@@ -3,12 +3,7 @@ import styled from "styled-components";
 
 function FindId() {
   const [tel, setTel] = useState("");
-  const idInputRef = useRef(null);
-
-  const handleClick = async (e) => {
-    setTel(e.target.value);
-    console.log(tel);
-  };
+  const [name, setName] = useState("");
 
   return (
     <LoginWrap id="login-wrap">
@@ -22,15 +17,25 @@ function FindId() {
           <br /> 인증받기를 통해 아이디를 알려드립니다.
         </InfoText>
         <InputBox className="id-box input-box">
+          <IdLabel htmlFor="name">이름</IdLabel>
+          <IdInput
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="회원 이름"
+            onChange={(e) => setName(e.target.value)}
+          />
+        </InputBox>
+        <InputBox className="id-box input-box">
           <IdLabel htmlFor="tel">전화번호</IdLabel>
           <IdInput
             type="text"
             id="tel"
             name="tel"
             value={tel}
-            ref={idInputRef}
             placeholder="가입할 때 입력한 휴대전화 번호"
-            onChange={handleClick}
+            onChange={(e) => setTel(e.target.value)}
             onInput={(e) => {
               e.target.value = e.target.value
                 .replace(/[^0-9.]/g, "")
@@ -39,8 +44,8 @@ function FindId() {
           />
         </InputBox>
         <ButtonBox className="submit-button-box button-box">
-          <Button isCheck={tel.length === 11} type="button">
-            인증받기
+          <Button isCheck={name && tel.length === 11} type="button">
+            문자 발송
           </Button>
         </ButtonBox>
       </LoginArea>
@@ -70,7 +75,7 @@ const InfoText = styled.div`
   width: 35%;
   font-size: 1rem;
   color: grey;
-  margin: 2rem auto 4rem auto;
+  margin: 2rem auto 3rem auto;
 `;
 
 const LoginHeader = styled.h1`
--- frontend/src/pages/FindPassword.js
+++ frontend/src/pages/FindPassword.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Link, useNavigate, useLocation } from "react-router-dom";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 
 function FindPassword() {
-  const [id, setId] = useState("");
-  const [password, setPassword] = useState("");
+  const [tel, setTel] = useState("");
+  const [name, setName] = useState("");
   const idInputRef = useRef(null);
-  const pwInputRef = useRef(null);
 
-
-  const handleClick = async () => {};
+  const handleClick = async (e) => {
+    setTel(e.target.value);
+    console.log(tel);
+  };
 
   return (
     <LoginWrap id="login-wrap">
@@ -17,13 +17,41 @@ function FindPassword() {
         <LoginHeaderBox className="login-box">
           <LoginHeader>아이디 찾기</LoginHeader>
         </LoginHeaderBox>
+        <InfoText>
+          가입 시 등록한 휴대폰 번호를 입력후
+          <br />
+          <br /> 인증받기를 통해 아이디를 알려드립니다.
+        </InfoText>
         <InputBox className="id-box input-box">
-          <IdLabel htmlFor="id">아이디</IdLabel>
-          <IdInput type="text" id="id" name="id" value={id} ref={idInputRef} />
+          <IdLabel htmlFor="name">이름</IdLabel>
+          <IdInput
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            placeholder="회원 이름"
+            onChange={handleClick}
+          />
+        </InputBox>
+        <InputBox className="id-box input-box">
+          <IdLabel htmlFor="tel">전화번호</IdLabel>
+          <IdInput
+            type="text"
+            id="tel"
+            name="tel"
+            value={tel}
+            placeholder="가입할 때 입력한 휴대전화 번호"
+            onChange={handleClick}
+            onInput={(e) => {
+              e.target.value = e.target.value
+                .replace(/[^0-9.]/g, "")
+                .replace(/(\..*)\./g, "$1");
+            }}
+          />
         </InputBox>
         <ButtonBox className="submit-button-box button-box">
-          <Button className="submit-button button" type="button">
-            로그인
+          <Button isCheck={tel.length === 11} type="button">
+            문자 발송
           </Button>
         </ButtonBox>
       </LoginArea>
@@ -44,8 +72,16 @@ const LoginArea = styled.div`
 `;
 
 const LoginHeaderBox = styled.div`
-  width: 50%;
+  width: 35%;
   margin: auto;
+  border-bottom: 2px solid black;
+`;
+
+const InfoText = styled.div`
+  width: 35%;
+  font-size: 1rem;
+  color: grey;
+  margin: 2rem auto 3rem auto;
 `;
 
 const LoginHeader = styled.h1`
@@ -68,16 +104,6 @@ const IdLabel = styled.label`
   margin: 10px 0px;
   font-size: 1rem;
   font-weight: 600;
-  color: ${(props) => !props.isIdCheck && "red"};
-`;
-
-const PwLabel = styled.label`
-  text-align: left;
-  display: block;
-  margin: 10px 0px;
-  font-size: 1rem;
-  font-weight: 600;
-  color: ${(props) => !props.isPwCheck && "red"};
 `;
 
 const IdInput = styled.input`
@@ -87,28 +113,10 @@ const IdInput = styled.input`
   font-size: 1.1rem;
   box-sizing: border-box;
   border: none;
-  border-bottom: ${(props) =>
-    !props.isIdCheck ? "1px solid red" : "1px solid black"};
+  border-bottom: 1px solid black;
   outline: none;
   :focus {
-    border-bottom: ${(props) =>
-      !props.isIdCheck ? "2px solid red" : "2px solid black"};
-  }
-`;
-
-const PwInput = styled.input`
-  padding: 11px 0px;
-  width: 100%;
-  height: 40px;
-  font-size: 1.1rem;
-  box-sizing: border-box;
-  border: none;
-  border-bottom: ${(props) =>
-    !props.isPwCheck ? "1px solid red" : "1px solid black"};
-  outline: none;
-  :focus {
-    border-bottom: ${(props) =>
-      !props.isPwCheck ? "2px solid red" : "2px solid black"};
+    border-bottom: 2px solid black;
   }
 `;
 
@@ -119,7 +127,7 @@ const ButtonBox = styled.div`
   align-items: center;
   text-align: center;
   width: 50%;
-  margin: 20px auto;
+  margin: 3rem auto;
   padding: 0;
 `;
 
@@ -128,81 +136,9 @@ const Button = styled.button`
   padding: 1rem;
   width: 70%;
   border: 0px;
-  background-color: ${(props) => (props.isCheck ? "black" : "grey")};
+  background-color: ${(props) => (props.isCheck ? "black" : "lightgrey")};
   color: white;
   margin: 10px auto;
   font-size: 1rem;
   font-weight: 500;
-`;
-
-const TextButtonBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  text-align: center;
-  width: 30%;
-  margin: 0 auto;
-  padding: 0;
-  text-decoration: none;
-`;
-
-const LoginLink = styled(Link)`
-  text-decoration: none;
-  color: black;
-`;
-
-const InfoBox = styled.div`
-  margin: 30px auto 20px auto;
-  text-align: center;
-  width: 50%;
-`;
-
-const ImgButtonBox = styled.div`
-  cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  -webkit-box-pack: justify;
-  justify-content: center;
-  -webkit-box-align: center;
-  align-items: center;
-  text-align: center;
-  width: 50%;
-  margin: 20px auto;
-  padding: 0px;
-  text-decoration: none;
-  > div {
-    display: flex;
-    width: 70%;
-    font-size: 1.1rem;
-    box-sizing: border-box;
-    font-weight: 700;
-    height: 52px;
-    border-radius: 12px;
-    border: 1px solid grey;
-  }
-`;
-
-const ImgButton = styled.img`
-  padding-left: 10px;
-  width: 50px;
-  height: 50px;
-  position: absolute;
-`;
-
-const ImgButtonText = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 50px;
-`;
-
-const WarningText = styled.div`
-  display: block;
-  position: absolute;
-  line-height: 16px;
-  font-size: 0.8rem;
-  color: #f15746;
 `;

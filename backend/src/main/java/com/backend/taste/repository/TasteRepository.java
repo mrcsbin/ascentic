9aebcbf2fa2a02cbaaef29c4dfb868733ec66138
kmindev <kangkm0324@gmailcom>
2023-06-08 01:36:41 +0900

    Fix: "취향테스트를 두번한 이용자의 결과를 조회할 때는 마지막 테스트 결과를 조회하도록 수정"

--- backend/src/main/java/com/backend/taste/repository/TasteRepository.java
+++ backend/src/main/java/com/backend/taste/repository/TasteRepository.java
@@ -2,10 +2,13 @@ package com.backend.taste.repository;
 
 import com.backend.member.entity.Member;
 import com.backend.taste.entity.Taste;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TasteRepository extends JpaRepository<Taste, Integer> {
-    Optional<Taste> findByMemberId(String memberId);
+    @Query("SELECT t FROM Taste t WHERE t.memberId = :memberId ORDER BY t.tasteNum DESC limit 1")
+    Optional<Taste> findLastByMemberId(@Param("memberId") String memberId);
 }

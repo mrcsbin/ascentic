b52d0a9e407ad4493a24fa3e6646ae66cb9c51f4
NaSung <2201skgotjd@gmail.com>
2023-06-07 23:38:28 +0900

    feat : 기존 ID찾기 -> 새로운 ID 찾기 이식 80%

--- frontend/src/pages/FindId.js
+++ frontend/src/pages/FindId.js
@@ -1,10 +1,76 @@
 import React, { useRef, useState } from "react";
 import styled from "styled-components";
+import { findId, findPw, sendCode, checkCode } from "../api/MemberApi";
+import Timer from "../components/common/Timer";
+import { SIGNUP_ERROR_MESSAGE } from "../constants/Message";
 
 function FindId() {
   const [tel, setTel] = useState("");
   const [name, setName] = useState("");
+  const [code, setCode] = useState("");
+  const [getData, setGetData] = useState("");
+  const [showCertificate, setShowCertificate] = useState(false);
+  const [codeOk, setCodeOk] = useState(false);
 
+  const getCode = async (phone) => {
+    const res = await sendCode(phone);
+    console.log(res);
+    if (res) {
+      setShowCertificate(true);
+    } else {
+      setShowCertificate(false);
+      setTel("");
+    }
+  };
+
+  const check = async (phone, code) => {
+    const res = await checkCode(phone, code);
+    if (res === "duplicateNum") {
+      alert("인증이 완료되었습니다!");
+      setCodeOk(true);
+      setShowCertificate(false);
+    } else {
+      if (res === "Wrong") {
+        alert(SIGNUP_ERROR_MESSAGE.PHONE_CERTIFICATE_WRONG);
+      } else if (res === "Ok") {
+        alert(SIGNUP_ERROR_MESSAGE.NO_SUCH_PHONE);
+        setShowCertificate(false);
+        setCodeOk(false);
+        setTel("");
+      } else {
+        alert(SIGNUP_ERROR_MESSAGE.UNKNOWN);
+        setShowCertificate(false);
+        setCodeOk(false);
+        setTel("");
+      }
+      setCodeOk(false);
+      setCode("");
+    }
+  };
+
+  const handleClick = async () => {
+    if (name === "") {
+      alert("이름을 입력해주세요");
+      return false;
+    }
+    if (tel === "") {
+      alert("전화번호를 입력해주세요");
+      return false;
+    }
+    if (!codeOk) {
+      alert("전화번호 인증을 완료해주세요");
+    }
+    const id = await findId(name, tel);
+    setGetData(id);
+  };
+
+  const handleCheckTime = () => {
+    alert(SIGNUP_ERROR_MESSAGE.PHONE_TIMEOVER);
+    setShowCertificate(false);
+    setCodeOk(false);
+    setCode("");
+    setTel("");
+  }; //Timer 시간 만료시 행동
   return (
     <LoginWrap id="login-wrap">
       <LoginArea className="login-area">
@@ -34,6 +100,7 @@ function FindId() {
             id="tel"
             name="tel"
             value={tel}
+            disabled={showCertificate || codeOk}
             placeholder="가입할 때 입력한 휴대전화 번호"
             onChange={(e) => setTel(e.target.value)}
             onInput={(e) => {
@@ -44,10 +111,39 @@ function FindId() {
           />
         </InputBox>
         <ButtonBox className="submit-button-box button-box">
-          <Button isCheck={name && tel.length === 11} type="button">
+          <Button
+            isCheck={name && tel.length === 11}
+            disabled={showCertificate || tel.length < 11 || codeOk}
+            onClick={() => getCode(tel)}
+            type="button"
+          >
             문자 발송
           </Button>
         </ButtonBox>
+        {showCertificate && (
+          <div className="certificateCode-box">
+            <br />
+            <div className="timer">
+              <Timer
+                timerActive={showCertificate}
+                onCheckTime={handleCheckTime}
+              />
+            </div>
+            <input
+              type="text"
+              name="code"
+              placeholder="인증번호"
+              value={code}
+              disabled={false}
+              onChange={(e) => setCode(e.target.value)}
+            />
+            <div className="button-box">
+              <button className="check" onClick={() => check(tel, code)}>
+                인증번호 확인하기
+              </button>
+            </div>
+          </div>
+        )}
       </LoginArea>
     </LoginWrap>
   );
